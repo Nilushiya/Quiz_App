05fd6fb0f1634ef616ee32f240675fbbcf66183a
Nilushiya <nilushiyak@gmail.com>
2024-07-12 15:00:18 +0530

    create QuestionCart component

---
+++ src/API.tsx
@@ -0,0 +1,10 @@
+export enum Difficulty{
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard"
+}
+
+export const fetchQuizQestions = async(amount : number , difficulty : Difficulty) =>{
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
+    
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuestionCard from './components/QuestionCard';
+
+const Total_Questions = 10
 
 function App() {
+  const [loading , setLoading] = useState(false)
+  const [questions , setQuestins] = useState([])
+  const [number , setNumber] = useState(0)
+  const [userAnswers , setUserAnswers] = useState([])
+  const [score , setScore] = useState(0)
+  const [gameOver , setGameOver] = useState(true)
+  const startTrivia = async() => {
+
+  }
+
+  const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const NextQuestion = () => {
+
+  }
   return (
     <div className="App">
-      
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score : </p>
+      <p className='L'>Loading Question....</p>
+      <QuestionCard 
+        questionNr={number + 1}
+        totalQuestions={Total_Questions}
+        question= {questions[number].question}
+        answers={questions[number].answers}
+        userAnswer = {userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className='next' onClick={NextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
